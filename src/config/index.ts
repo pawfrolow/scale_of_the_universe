02c27257fc0089889ebbc0d3ebdebbf6da6b6801
paw.frolow <paw.frolow@gmail.com>
2026-03-16 16:54:37 +0300

    new items

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,30 @@ export const MIN_SCALE_EXP = -35
 export const MAX_SCALE_EXP = 27
 
 export const MAX_COMMON_TEXTURES = 329
+
+export const LANGUAGE_STORAGE_KEY = 'sotu_language';
+export const MUTED_STORAGE_KEY = 'sotu_muted';
+
+export const LANGUAGE_OPTIONS = [
+  { code: 'ru', label: 'Русский' },
+  { code: 'en', label: 'English (US)' },
+  { code: 'en-GB', label: 'English (UK)' },
+  { code: 'he', label: 'עברית' },
+  { code: 'nl', label: 'Nederlands' },
+  { code: 'zh-CH', label: '中文(简体)' },
+  { code: 'es', label: 'Español' },
+  { code: 'sv', label: 'Svenska' },
+  { code: 'pl', label: 'Polski' },
+  { code: 'pt', label: 'Português' },
+  { code: 'de', label: 'Deutsch' },
+  { code: 'zh-Hant', label: '中文(繁體)' },
+  { code: 'fr', label: 'Français' },
+  { code: 'eo', label: 'Esperanto' },
+  { code: 'ro', label: 'Romanian' },
+  { code: 'ua', label: 'Українська' },
+  { code: 'ar', label: 'العربية' },
+  { code: 'ko', label: '한국어' },
+  { code: 'et', label: 'Eesti' },
+  { code: 'fa', label: 'فارسی' },
+  { code: 'tr', label: 'Türkçe' },
+];
